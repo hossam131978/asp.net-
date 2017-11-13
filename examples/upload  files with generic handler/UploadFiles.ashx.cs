a1bc4e10405358201f0ffd98190aa358a95e88f1
hossam <hossam131978@gmail.com>
2017-11-13 22:14:28 +0200

    creating asp.net folder
    the repository includes
    asp.net projects
    mvc projects
    general examples

---
+++ examples/upload  files with generic handler/UploadFiles.ashx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace uploadfiles
+{
+    /// <summary>
+    /// Summary description for UploadFiles1
+    /// </summary>
+    public class UploadFiles1 : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string upload_directory = "uploaded_files";
+            string physical_upload_directory =context.Server.MapPath( "~/") + upload_directory;
+
+            if (Directory.Exists(physical_upload_directory))
+            {
+                physical_upload_directory = context.Server.MapPath("~/" + upload_directory + "/");
+            }
+            else
+            {
+                Directory.CreateDirectory(context.Server.MapPath("~/")+upload_directory);
+                physical_upload_directory = context.Server.MapPath("~/" + upload_directory + "/");
+            }
+            try
+            {
+                var files = context.Request.Files;
+                HttpPostedFile file;
+                for (int i = 0; i < files.Count; i++)
+                {
+                    file = files[i];
+                    //getting the file name  'cutting the path'
+                    var file_name="";
+                    if (file.FileName.IndexOf("\\") != -1)
+                    {
+                        var index1 = file.FileName.LastIndexOf("\\");
+                        var index2 = file.FileName.Length;
+                            file_name = file.FileName.Substring(index1 + 1, index2 - index1 - 1);
+                    }
+                    var file_to_save = physical_upload_directory + file_name;
+                    //saving the file
+                    files[i].SaveAs(file_to_save);
+                }
+                var directory2 = context.Server.MapPath("~/");
+                //  var xcs2 = Directory.GetFiles("~/"+upload_directory);
+                var files_in_upload_directory =String.Join(" , ", (Directory.GetFiles(physical_upload_directory)));
+                 
+                context.Response.Write(files_in_upload_directory);
+            }
+            catch (Exception ex)
+            {
+
+                context.Response.Write(ex.Message);
+            }
+
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ examples/upload  files with generic handler/WebForm1.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="uploadfiles.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    
+    <script src="Scripts/jquery-1.11.2.js"></script>
+   
+
+    <%--upload files using ajax  --%> 
+    <script type="text/javascript">
+        var text;
+        $(document).ready(function () {
+            $("#upload_files").click(function ()
+            {
+                var files = $("#select_files")[0].files;
+                if (files.length > 0)
+                {
+                    var data = new FormData();
+                    for (var i = 0; i < files.length; i++)
+                    {
+                        data.append(files[i].name, files[i])
+                    }
+                    $.ajax({
+                            url: "UploadFiles.ashx",
+                            method:"post",
+                            contentType: false,
+                            processData: false,
+                            data: data,
+                            error: function (data) {alert("Error  " + data.status);
+                            },
+                            success: function (data) {
+                                text = data.replace(/,/g, "<br/>");
+                               $('#result').html(text);
+                            }
+                        })
+                }
+            })
+        });
+        
+        
+       
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <input type="file" id="select_files" multiple="multiple" />
+            <input type="button" id="upload_files" value="upload files"  />
+             <p id="result"> </p>
+
+        </div>
+    </form>
+   
+</body>
+
+</html>
